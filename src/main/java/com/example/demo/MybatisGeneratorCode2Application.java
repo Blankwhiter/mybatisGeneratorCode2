d55a753e892bd56f049dde609f790dd19bf75c9f
Blankwhiter <belonghuang@126.com>
2018-06-21 14:34:48 +0800

    mybatis通过maven插件生成代码

---
+++ src/main/java/com/example/demo/MybatisGeneratorCode2Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisGeneratorCode2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisGeneratorCode2Application.class, args);
+	}
+}
---
+++ src/main/java/com/example/demo/po/SysDept.java
@@ -0,0 +1,48 @@
+package com.example.demo.po;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class SysDept implements Serializable {
+    private Long id;
+
+    private String deptName;
+
+    private Date createTime;
+
+    private String deptCode;
+
+    private static final long serialVersionUID = 1L;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName == null ? null : deptName.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getDeptCode() {
+        return deptCode;
+    }
+
+    public void setDeptCode(String deptCode) {
+        this.deptCode = deptCode == null ? null : deptCode.trim();
+    }
+}
